2edf476332a567cafcb6561ce49b762aad6c2aba
Gabriel Poça <gabrielpoca@gmail.com>
2014-11-18 13:59:51 +0000

    Adds support for over animations

--- example/example.js
+++ example/example.js
@@ -11,6 +11,14 @@ if (Meteor.isClient) {
     name: 'rightPage'
   });
 
+  Router.route('/over_up', {
+    name: 'overUp'
+  });
+
+  Router.route('/over_down', {
+    name: 'overDown'
+  });
+
   Router.route('/left', {
     name: 'leftPage'
   });
@@ -21,22 +29,40 @@ if (Meteor.isClient) {
 
   Template.firstPage.events({
     'click #goRight': function() {
-      IronBender.go('/right', {}, { animation: 'slideOverUpClose' });
+      IronBender.go('/right', {}, { animation: 'slideLeft' });
     },
     'click #goLeft': function() {
-      IronBender.go('/left', {}, { animation: 'slideOverDownClose' });
+      IronBender.go('/left', {}, { animation: 'slideRight' });
+    },
+    'click #overDown': function() {
+      IronBender.go('/over_down', {}, { animation: 'slideOverDown' });
+    },
+    'click #overUp': function() {
+      IronBender.go('/over_up', {}, { animation: 'slideOverUp' });
+    }
+  });
+
+  Template.overDown.events({
+    'click #back': function() {
+      IronBender.go('/', {}, { animation: 'slideOverDownClose' });
+    }
+  });
+
+  Template.overUp.events({
+    'click #back': function() {
+      IronBender.go('/', {}, { animation: 'slideOverUpClose' });
     }
   });
 
   Template.leftPage.events({
     'click #back': function() {
-      IronBender.go('/', {}, { animation: 'slideRight' });
+      IronBender.go('/', {}, { animation: 'slideLeft' });
     }
   });
 
   Template.rightPage.events({
     'click #back': function() {
-      IronBender.go('/', {}, { animation: 'slideLeft' });
+      IronBender.go('/', {}, { animation: 'slideRight' });
     }
   });
 }
